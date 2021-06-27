e921fcd9714abf6ad3c8053f08267225b5556de4
Sukhesh Gopaluni <sgopaluni@stratacache.com>
2021-06-27 13:42:15 -0400

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { animate, state, style, transition, trigger } from '@angular/animations';
+import { HttpClient } from '@angular/common/http';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class AppComponent {
-  title = 'pokemon-ui';
+  public title = 'pokemon-ui';
+  public dataSource = new MatTableDataSource();
+  public columnsToDisplay = ['name', 'url'];
+  public expandedElement: null = null;
+  public detailObj: any = {};
+
+
+  @ViewChild(MatPaginator) paginator: MatPaginator | null = null;
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit() {
+    this.getPokemonList()
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  getPokemonList() {
+    this.http.get("https://pokeapi.co/api/v2/pokemon?limit=10&offset=1").subscribe((res: any) => {
+      this.dataSource = new MatTableDataSource(res.results);
+    })
+  }
+
+  getPokemonByName(name: String){
+    this.http.get(`https://pokeapi.co/api/v2/pokemon/${name}`).subscribe((res: any) => {
+      console.log(res)
+      this.detailObj = res;
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
+import {  MatPaginatorModule } from '@angular/material/paginator';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTableModule } from '@angular/material/table';
+import {MatGridListModule} from '@angular/material/grid-list';
+import { HttpClientModule } from '@angular/common/http';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatTableModule,
+    HttpClientModule,
+    MatPaginatorModule,
+    MatGridListModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
